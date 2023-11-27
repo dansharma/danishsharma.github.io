16fe3196dbaa5b5c06cfba6bed824ba11292bece
dansharma <145412834+dansharma@users.noreply.github.com>
2023-11-27 17:41:32 -0500

    commit-3
    ball class maded and a draw method added .

--- assignment-4/part-3/main.js
+++ assignment-4/part-3/main.js
@@ -14,7 +14,28 @@ function random(min, max) {
 }
 
 // function to generate random color
-
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+// creating a class named ball so that that we can have multiple balls as its object and intilizing some of its properties
+class Ball {
+  constructor(x, y, velocityX, velocityY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velocityX = velocityX;
+    this.velocityY = velocityY;
+    this.color = color;
+    this.size = size;
+  }
+  // creating a method named draw inside the ball class which will draw a ball object using a html canvas function
+  drawball() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * 3.14,true);
+    ctx.fill();
+  }
+
+
+
+}
+
